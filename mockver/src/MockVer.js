5d24a226b36229d601faa394db914b1aaedd7043
RoronuaHong <807672412@qq.com>
2022-08-05 21:30:40 +0800

    initial

---
+++ mockver/src/MockVer.js
@@ -0,0 +1,12 @@
+import './MockVer.css'
+import mainContainer from './components/mainContainer/index'
+
+function App() {
+  return (
+    <div className="App">
+      <mainContainer />
+    </div>
+  )
+}
+
+export default App
